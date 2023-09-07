a69138ec103a20de1caab5bbade818cc90541d0d
Demkovychkate <Panchevakate1804@gmail.com>
2023-09-07 18:17:22 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let btn1 = document.querySelector('.btn-one');
+
+function FirstTask(){
+let name = prompt ('What is your name?',John);
+alert ( 'Hello, '+name+' How are you? ');
+}
+
+btn1.onclick= FirstTask;
