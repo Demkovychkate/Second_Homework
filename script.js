ec886b202aac3a3fd471fa5375b75ae86f053999
Demkovychkate <Panchevakate1804@gmail.com>
2023-09-07 18:25:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let btn1 = document.querySelector('.btn-one');
 
 function FirstTask(){
-let name = prompt ('What is your name?',John);
+let name = prompt ('What is your name?','John');
 alert ( 'Hello, '+name+' How are you? ');
 }
 
